078dc802a4ec7baae8111052eb519a61975afa95
James Fox <jimmyfox00@gmail.com>
2022-07-25 22:27:24 -0700

    Removed console.logs from myBGService

--- Api/Services/MyBackgroundService.cs
+++ Api/Services/MyBackgroundService.cs
@@ -24,19 +24,13 @@ namespace Api.Services
             {
                 using(var scope = _serviceProvider.CreateScope())
                 {
-                    // DateTime today = DateTime.Now;
-                    DateTime today = new DateTime(2022, 9, 1); 
+                    DateTime today = DateTime.Now;
                     if(today.Day == 1)
                     {
-                        Console.WriteLine("Starting Background Services");
                         var _userBackgroundService = scope.ServiceProvider.GetRequiredService<UserBackgroundService>();
                         var _frequentBackgroundService = scope.ServiceProvider.GetRequiredService<FrequentBackgroundService>();
                         await _userBackgroundService.UpdateUserStatus();
-                        Console.WriteLine("Updated Users");
-                        Console.WriteLine("Starting to update Frequents");
                         await _frequentBackgroundService.CreateFrequentExpenses(today);
-                        Console.WriteLine("Updated Frequents");
-                        Console.WriteLine("Finished Background Services");
                     }
                     await Task.Delay(TimeSpan.FromHours(24), stoppingToken);
                 }
